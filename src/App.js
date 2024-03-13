edc23601d845abb09d1de534166c94964f8c1778
vibhanshu2025 <sharmavibhanshu80@gmail.com>
2024-03-13 23:57:20 +0530

    Items page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductGrid from './components/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductGrid />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import './Navbar.css'
+
+export const Navbar = () => {
+    return (<nav className="navbar navbar-expand-custom navbar-mainbg">
+    <a className="navbar-brand navbar-logo" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <i className="fas fa-bars text-white"></i>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+            <div className="hori-selector"><div className="left"></div><div className="right"></div></div>
+            <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0);"><i className="fas fa-tachometer-alt"></i>Dashboard</a>
+            </li>
+            <li className="nav-item active">
+                <a className="nav-link" href="javascript:void(0);"><i className="far fa-address-book"></i>Address Book</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0);"><i className="far fa-clone"></i>Components</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0);"><i className="far fa-calendar-alt"></i>Calendar</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0);"><i className="far fa-chart-bar"></i>Charts</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0);"><i className="far fa-copy"></i>Documents</a>
+            </li>
+        </ul>
+    </div>
+</nav>)
+
+}
